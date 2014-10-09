d7ea20f42df2911488e6fb1bb00962f2a755a2fb
Jennifer Ling <j3c8@ugrad.cs.ubc.ca>
2014-10-08 22:17:10 -0700

    Finished rough draft of BFS.cpp

--- Breadth-First Search/bfs.cpp
+++ Breadth-First Search/bfs.cpp
@@ -1,4 +1,46 @@
-#include <bfs.h>
+#include "bfs.h"
+#include <queue>
+#include <iostream>
 
 using namespace std;
 
+queue<Node*> frontier;	// queue of nodes to check to find goal
+
+
+int main() {
+	return 0;
+}
+
+
+void BFS(Node* starting_node, int goal_val) {
+	
+	// check for null graph
+	if (starting_node == 0) {
+		cout << "Graph does not exist" << endl;
+		return;
+	}
+	
+	// otherwise put it into the frontier
+	frontier.push(starting_node);
+	
+	Node* currentNode = 0;
+	
+	// 
+	while (!frontier.empty()) {
+		currentNode = frontier.front();
+		if (CheckForGoal(currentNode, goal_val)) { 
+			return; 
+		}
+		else { 
+			frontier.push(currentNode->left_child);
+			frontier.push(currentNode->right_child);
+			frontier.pop(); 
+		}
+	}
+	
+	cout << "No goal found" << endl;
+	
+	}
+}
+
+bool CheckForGoal(Node* node, int goal);
--- Breadth-First Search/bfs.h
+++ Breadth-First Search/bfs.h
@@ -7,7 +7,17 @@ struct Node {
 	int value;
 };
 
+Node* NodeConstructor(Node* p, Node* lc, Node* rc, int val) {
+	Node* this_node = 0;
+	(*this_node).parent = p;
+	(*this_node).left_child = lc;
+	(*this_node).right_child = rc;
+	(*this_node).value = val;
+	return this_node;
+}
+
 void BFS(Node* starting_node, int goal_val);
 
 bool CheckForGoal(Node* node, int goal);
 
+// create graph here
